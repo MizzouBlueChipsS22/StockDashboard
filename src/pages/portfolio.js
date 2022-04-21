e7cd7bebd9c067a43b97366e0e8a87b5d5164d51
Caleb Sellinger <70022402+crsellinger@users.noreply.github.com>
2022-04-21 17:06:10 -0500

    Start

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -6,14 +6,66 @@
  */
 import Head from 'next/head';
 import { DashboardLayout } from '../components/dashboard-layout';
+import React from 'react';
+import { Container, Grid } from '@mui/material';
+import { Box } from '@mui/system';
 
 const PortfolioRoot = () => (
     <>
       <Head>
         <title>
-          User Account | Material Kit
+          User Portfolio
         </title>
       </Head>
+      <Grid 
+        container 
+        spacing={2}
+        direction="column"
+        justifyContent="left"
+        alignItems="center"
+      >
+        <Grid item rowSpacing={2} md={2}>
+          <Box
+            sx={{
+              backgroundColor: 'lightslategrey'
+            }}
+          >
+            <Container>
+                1
+            </Container>
+          </Box>
+        </Grid>
+        <Grid item md={2}>
+          <Box
+            sx={{
+              backgroundColor: 'powderblue'
+            }}
+          >
+            <Container>
+                2
+            </Container>
+          </Box>
+        </Grid>
+      </Grid>
+      <Grid
+        container
+        spacing={2}
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Grid item rowSpacing={2} md={2}>
+          <Box
+            sx={{
+              backgroundColor: 'lightslategrey'
+            }}
+          >
+            <Container>
+                3
+            </Container>
+          </Box>
+        </Grid>
+      </Grid>
     </>
   );
   
