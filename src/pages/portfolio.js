df087c9e8ea1a1908172cc2b3dacf26888731afd
Caleb Sellinger <70022402+crsellinger@users.noreply.github.com>
2022-04-21 14:13:38 -0500

    Update portfolio.js

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -6,6 +6,9 @@
  */
 import Head from 'next/head';
 import { DashboardLayout } from '../components/dashboard-layout';
+import React from 'react';
+import { Box } from '@mui/system';
+import { Container } from '@mui/material';
 
 const PortfolioRoot = () => (
     <>
@@ -14,6 +17,63 @@ const PortfolioRoot = () => (
           User Account | Material Kit
         </title>
       </Head>
+      <Box
+        component="main"
+        sx={{
+          alignItems: 'right',
+          display: 'flex',
+          flexGrow: 1,
+          minHeight: '100%',
+        }}
+      >
+        <Container maxWidth="md">
+          First Container
+        <Box
+          sx={{
+            alignItems: 'center',
+            display: 'flex',
+            flexDirection: 'column',
+            backgroundColor: "blue"
+          }}
+        >
+          First
+        </Box>
+        <Box
+          sx={{
+            alignItems: 'center',
+            display: 'flex',
+            flexDirection: 'column',
+            backgroundColor: "darkcyan"
+          }}
+        >
+          Second
+        </Box>
+        <Box
+          sx={{
+            alignItems: 'center',
+            display: 'flex',
+            flexDirection: 'column',
+            backgroundColor: "white"
+          }}
+        >
+          Third
+        </Box>
+        </Container>
+        <Container maxWidth="md">
+          second container
+          <Box
+            sx={{
+              alignItems: 'center',
+              display: 'flex',
+              flexDirection: 'column',
+              backgroundColor: "cadetblue",
+              flexGrow: "1"
+            }}
+          >
+            Portfolio Pie Chart
+          </Box>
+        </Container>
+      </Box>
     </>
   );
   
